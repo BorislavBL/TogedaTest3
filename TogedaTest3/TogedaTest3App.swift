2ee1cce8b98e4e765cdedceb1cb872f9df2e1bf2
BorislavBL <borkolorinkov@gmail.com>
2023-09-15 19:07:22 +0300

    Initial Commit

---
+++ TogedaTest3/TogedaTest3App.swift
@@ -0,0 +1,17 @@
+//
+//  TogedaTest3App.swift
+//  TogedaTest3
+//
+//  Created by Borislav Lorinkov on 15.09.23.
+//
+
+import SwiftUI
+
+@main
+struct TogedaTest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
